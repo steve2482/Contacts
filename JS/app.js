3cb3ab842b2c24f4c5d9ee3a8fdda739c0db47f2
steve2482 <stevecb12684@yahoo.com>
2016-08-20 11:18:42 -0400

    Add phone number button-works, second number wont add to contact

--- JS/app.js
+++ JS/app.js
@@ -3,10 +3,12 @@ $(document).ready(function() {
   var id = 0;
   var firstName = $("#first-name");
   var lastName = $("#last-name");
-  var phone = $("#phone");
+  var phone = $("#phone0");
+  var phoneNumbers = [];
   var street = $("#street");
   var city = $("#city");
   var state = $("#state");
+  var address = [street, city, state];
 
   // Storage Array
 
@@ -17,9 +19,7 @@ $(document).ready(function() {
     this.firstName = firstName;
     this.lastName = lastName;
     this.phone = phone;
-    this.street = street;
-    this.city = city;
-    this.state = state;
+    this.address = address;
   }
 
   // Form validation and submission
@@ -27,7 +27,8 @@ $(document).ready(function() {
   function validateForm() {
     var x = $('#first-name').val();
     var y = $('#last-name').val();
-    var z = $('#phone').val();
+    var z = $('#phone0').val();
+    // Validate Entry
     if (x === null || x === '' || x === undefined) {
       $('.more-info-modal').show();
     } else if (y === null || y === '' || y === undefined) {
@@ -35,11 +36,16 @@ $(document).ready(function() {
     } else if (z === null || z === '' || z === undefined) {
       $('.more-info-modal').show();
     } else {
-      var contact = new Contact(id, firstName.val(), lastName.val(), phone.val(), street.val(), city.val(), state.val());
+      // Create Contact
+      for (var i = 0; i < phoneNumbers.length; i++) {
+        phone.push(phoneNumbers[i].val());
+      }
+      var contact = new Contact(id, firstName.val(), lastName.val(), phoneNumbers, street.val(), city.val(), state.val());
       contactList.push(contact);
       $("#contact-names").append(
           '<li class="names"><a href="#" id="' + contact.id + '">' + contact.firstName + ' ' + contact.lastName + "</a></li>");
       id++;
+      // Clear Form
       document.getElementById("contact-form").reset();
     }
   }
@@ -51,26 +57,46 @@ $(document).ready(function() {
     validateForm();
   });
 
+  // Add Additional Phone Number
+
+  $('#add-phone').click(function(e) {
+    e.preventDefault();
+    $('#phone-container').append(
+      '<input id="phone' + phoneNumbers.length + '" class="form-fields" type="tel"><br>');
+  });
+
   // Click OK on modal to hide
 
   $('.info-ok-button').click(function() {
     $('.more-info-modal').hide();
   });
 
-  // Click Contact Name to Display Contact Info
+  // Click Contact Name to Display Contact Info and to Edit
 
   $('#contact-names').click('a', function(e) {
     var id = e.target.id;
-    console.log(e.target, e.target.id);
+    console.log(contactList[id]);
+    // Display contact
     $('#fName-display').text(
       contactList[id].firstName + ' ' + contactList[id].lastName);
     $('#phone-display').text(
-      contactList[id].phone);
-    $('#street-display').text(
-      contactList[id].street);
-    $('#city-display').text(
-      contactList[id].city);
-    $('#state-display').text(
-      contactList[id].state);
+      contactList[id].phoneNumbers[0]);
+    $('#address-display').text(
+      contactList[id].address);
+    // Fill in Form to Allow for Edit
+    $('#first-name').val(
+      contactList[id].firstName);
+    $('#last-name').val(
+      contactList[id].lastName);
+    for (var i = 0; i < phoneNumbers.length; i++) {
+      $('#phone' + i).val(
+        contactList[id].phoneNumbers[i]);
+    }
+    $('#street').val(
+      contactList[id].address[0]);
+    $('#city').val(
+      contactList[id].address[1]);
+    $('#state').val(
+      contactList[id].address[2]);
   });
 });
